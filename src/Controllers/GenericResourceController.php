981826d13c38d8542c19b77f76504fca5cdeaede
Nick <parsnick@users.noreply.github.com>
2015-11-26 19:26:33 +0000

    Added generic resource controller scaffold + route macro

---
+++ src/Controllers/GenericResourceController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Parsnick\EloquentJs\Controllers;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class GenericResourceController extends Controller
+{
+    /**
+     * @var Model
+     */
+    protected $model;
+
+    /**
+     * Set the model for this resource controller.
+     *
+     * @param Model $model
+     */
+    public function setModel(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return $this->model->useEloquentJs()->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- src/EloquentJsServiceProvider.php
+++ src/EloquentJsServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace Parsnick\EloquentJs;
 
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Routing\Router;
 use Illuminate\Support\ServiceProvider;
+use Parsnick\EloquentJs\Controllers\GenericResourceController;
 use Parsnick\EloquentJs\Events\EloquentJsWasCalled;
 use Parsnick\EloquentJs\Listeners\ApplyQueryMethodsToBuilder;
 use Parsnick\EloquentJs\Translators\JsonQueryTranslator;
@@ -18,17 +21,63 @@ class EloquentJsServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind(QueryTranslator::class, function ($app) {
-           return new JsonQueryTranslator($app['request']->input('query'));
+           return new JsonQueryTranslator($app['request']->input('query', '[]'));
         });
     }
 
     /**
      * Boot the service provider.
      *
+     * @param Dispatcher $events
+     * @param Router $router
      * @return void
      */
-    public function boot()
+    public function boot(Dispatcher $events, Router $router)
     {
-        $this->app['events']->listen(EloquentJsWasCalled::class, ApplyQueryMethodsToBuilder::class);
+        $events->listen(EloquentJsWasCalled::class, ApplyQueryMethodsToBuilder::class);
+
+        $this->addGenericResourceRouting($router);
+    }
+
+    /**
+     * Set up the generic resource routes + controller.
+     *
+     * @param Router $router
+     */
+    protected function addGenericResourceRouting(Router $router)
+    {
+        $router->macro('eloquent', function ($uri, $resource, $options = []) use ($router) {
+
+            // The $router->resource() method doesn't allow custom route attributes
+            // in the $options array. So, while the group() may look redundant here,
+            // we need it to attach the relevant resource classname to each of the
+            // individual restful routes being defined.
+            //
+            // This is so when we come to resolve the controller (see below), we
+            // can easily tell what type of resource we need, i.e. which model.
+            $router->group(compact('resource'), function ($router) use ($uri, $options) {
+
+                if (empty($options['only'])) { // Exclude the routes for displaying forms
+                    $options['except'] = (array) array_get($options, 'except', []) + ['create', 'edit'];
+                }
+
+                $router->resource($uri, '\\'.GenericResourceController::class, $options);
+
+            });
+        });
+
+        // Typically you'd have dedicated controllers for each resource.
+        // Since that's not the case here, we need some way of telling
+        // our generic controller which resource we're working with.
+        $this->app->resolving(function (GenericResourceController $controller) {
+
+            $currentRoute = $this->app['router']->current();
+
+            if ($currentRoute) { // in case we're in the console, etc.
+                $controller->setModel(
+                    $this->app->make($currentRoute->getAction()['resource'])
+                );
+            }
+        });
     }
 }
